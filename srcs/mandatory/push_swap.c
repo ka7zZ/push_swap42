f87e0446cce04a788733c829503759b74ef4042a
ka7z <@42campus>
2025-02-27 14:26:59 +0100

    done.

--- srcs/mandatory/push_swap.c
+++ srcs/mandatory/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aghergut <aghergut@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 10:20:09 by aghergut          #+#    #+#             */
-/*   Updated: 2025/02/26 19:00:51 by aghergut         ###   ########.fr       */
+/*   Updated: 2025/02/27 14:21:02 by aghergut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,8 @@ int	main(int argc, char **argv)
 {
 	t_list	*stack_a;
 	t_list	*stack_b;
-	int		moves;
 
-	if (argc < 1)
+	if (argc < 2)
 		return (ft_putstr_fd("Error\n", 1), 0);
 	stack_a = NULL;
 	stack_b = NULL;
@@ -28,7 +27,6 @@ int	main(int argc, char **argv)
 		return (ft_lstclear(&stack_a, free), 0);
 	if (ft_lstsize(stack_a) <= 5)
 		return (sort_min(&stack_a, &stack_b), ft_lstclear(&stack_a, free), 0);
-	moves = 0;
 	set_index(stack_a, ft_lstsize(stack_a), 0);
 	sort_large(&stack_a, &stack_b);
 	ft_lstclear(&stack_a, free);
