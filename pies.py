448bf97da0f923e8b8bd6215e898ce347500f548
ka7zZ <149519502+ka7zZ@users.noreply.github.com>
2025-03-07 08:34:33 +0100

    saw a new pattern

---
+++ pies.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as pl # type: ignore
+import numpy as np # type: ignore
+
+coins = ['ETH', 'TET', 
+         'RNDR', 'AKT', 'IO.NET', 'OCTA', 
+         'ASI', 'ALI', 'LAI', 'ARKI', 'GRAPH', 
+         'RIO', 'NXRA', 
+         'ANYONE', 
+         'BEAMX']
+
+values = [4445,98, ]
