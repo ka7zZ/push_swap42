508a7a7a9a85bad6a62fa9e5dd3eeb391f1cfeab
ka7zZ <149519502+ka7zZ@users.noreply.github.com>
2025-03-27 19:14:31 +0100

    done90%.

---
+++ srcs/mandatory/sort/sort_bigger.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_bigger.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aghergut <aghergut@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 11:19:33 by aghergut          #+#    #+#             */
+/*   Updated: 2025/03/07 16:45:58 by aghergut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../include/push_swap.h"
+/*
+static int	get_max(t_list *stack)
+{
+	t_list	*ptr;
+	t_body	*body_ptr;
+	int		res;
+
+	ptr = stack;
+	res = 0;
+	while (ptr)
+	{
+		body_ptr = (t_body *)ptr->content;
+		if (body_ptr->i > res)
+			res = body_ptr->i;
+		ptr = ptr->next;
+	}
+	return (res);
+}
+
+static unsigned int	count_bits(unsigned int n)
+{
+	unsigned int	count;
+
+	count = 0;
+	while (n)
+	{
+		count++;
+		n >>= 1;
+	}
+	return (count);
+}
+
+void	sort_bigger(t_list **stack_a, t_list **stack_b)
+{
+    t_body  *buf;
+    int     bigger;
+    
+    bigger = count_bits(get_max(*stack_a));
+    
+    
+}
+*/
